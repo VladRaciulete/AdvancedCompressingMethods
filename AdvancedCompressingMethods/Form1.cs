418d179fdd632d0073b920608bc521553b600515
DESKTOP-VMV1GB4\vladr <vladraciulete@yahoo.com>
2025-05-14 00:05:34 +0300

    Initial Wavelet commit.

--- AdvancedCompressingMethods/Form1.cs
+++ AdvancedCompressingMethods/Form1.cs
@@ -14,34 +14,6 @@ namespace AdvancedCompressingMethods
         {
         }
 
-        private async void button1_Click(object sender, EventArgs e)
-        {
-            Random random = new Random();
-            int randomNumber;
-
-            this.label1.Text = "started";
-            await Task.Delay(100);
-
-            while (true) {
-                randomNumber = random.Next(30, 33);
-
-                long bitsLeft = fileController.getRemainingBitsLeft();
-
-                if (randomNumber <= bitsLeft)
-                {
-                    int[] test = fileController.ReadNBits(randomNumber);
-                    fileController.WriteNBits(test, randomNumber);
-                }
-                else {
-                    int[] test = fileController.ReadNBits((int)bitsLeft);
-                    fileController.WriteNBits(test, (int)bitsLeft);
-                    break;
-                }
-            }
-            this.label1.Text = "done";
-        }
-
-
         ~Form1()
         {
             fileController.close();
