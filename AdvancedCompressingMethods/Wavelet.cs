249942bad6749522d670a090507de332334dced9
DESKTOP-VMV1GB4\vladr <vladraciulete@yahoo.com>
2025-05-14 00:44:17 +0300

    Created the Wavelet class. Added the analysis and synthesis vectors.

---
+++ AdvancedCompressingMethods/Wavelet.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCompressingMethods
+{
+    internal class Wavelet
+    {
+        private double[] analysisLow;
+        private double[] analysisHigh;
+        private double[] synthesisLow;
+        private double[] synthesisHigh;
+
+        public Wavelet()
+        {
+            this.analysisLow = [
+                0.026748757411,
+                -0.016864118443,
+                -0.078223266529,
+                0.266864118443,
+                0.602949018236,
+                0.266864118443,
+                -0.078223266529,
+                -0.016864118443,
+                0.026748757411
+                ];
+            this.analysisHigh = [
+                0.0,
+                0.091271763114,
+                -0.057543526229,
+                -0.591271763114,
+                1.115087052457,
+                -0.591271763114,
+                -0.057543526229,
+                0.091271763114,
+                0.0
+                ];
+            this.synthesisLow = [
+                0.0,
+                -0.091271763114,
+                -0.057543526229,
+                0.591271763114,
+                1.115087052457,
+                0.591271763114,
+                -0.057543526229,
+                -0.091271763114,
+                0.0
+                ];
+            this.synthesisHigh = [
+                0.026748757411,
+                0.016864118443,
+                -0.078223266529,
+                -0.266864118443,
+                0.602949018236,
+                -0.266864118443,
+                -0.078223266529,
+                0.016864118443,
+                0.026748757411
+                ];
+        }
+    }
+}
